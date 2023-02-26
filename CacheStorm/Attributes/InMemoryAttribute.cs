efc23a9f435bdd64e0b9b32b76b135fa561a8433
WojciechKurowski <kurowski.wojciechjan@gmail.com>
2023-02-26 21:14:08 +0100

    Initial commit

--- CacheStorm/Attributes/InMemoryAttribute.cs
+++ CacheStorm/Attributes/InMemoryAttribute.cs
@@ -3,14 +3,16 @@
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
 public class InMemoryAttribute : Attribute
 {
-    public TimeSpan ExpirationPeriod { get; set; } = TimeSpan.FromMinutes(30);
-    public InMemoryAttribute(TimeSpan expirationPeriod)
+    public TimeSpan ExpirationPeriod { get; } = TimeSpan.FromMinutes(30);
+    public string Key { get; }
+    public InMemoryAttribute(string key,TimeSpan expirationPeriod)
     {
+        Key = key;
         ExpirationPeriod = expirationPeriod;
     }
 
-    public InMemoryAttribute()
+    public InMemoryAttribute(string key)
     {
-
+        Key = key;
     }
 }
